71d565cbf830ea30e4cc0b538bac96ef70043f66
Gennadiy Metalnikov <met_gen@outlook.com>
2024-04-21 23:21:01 +0300

    fixed prettier formating

--- components/homepage/Greeting.tsx
+++ components/homepage/Greeting.tsx
@@ -1,7 +1,11 @@
 const Greeting = () => {
   return (
     <div className="lg:mb-10 lg:mt-3">
-      <h1 className="clip text-5xl font-extrabold text-transparent dark:to-blue-500">Привет!<br/>Добро пожаловать!</h1>
+      <h1 className="clip text-5xl font-extrabold text-transparent dark:to-blue-500">
+        Привет!
+        <br />
+        Добро пожаловать!
+      </h1>
     </div>
   );
 };
