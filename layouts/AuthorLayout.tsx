b25b0826e651e9d981df2e55f7025ecd8dff92cc
Gennadiy Metalnikov <met_gen@outlook.com>
2024-04-22 14:28:05 +0300

    Change h1 in authorlayout

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -34,7 +34,7 @@ export default function AuthorLayout({ children, content }: Props) {
       <div className="about divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Обо мне
+            О блоге
           </h1>
           <p className="text-base text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
             Кратко о себе и цели моего блога
