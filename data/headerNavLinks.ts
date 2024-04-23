185a1b630b8167a13e9988b3b3c90a185ba497c8
Gennadiy Metalnikov <met_gen@outlook.com>
2024-04-23 23:35:53 +0300

    add gallery to navlinks

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Блог' },
+  { href: 'https://gallery.metalnikovg.ru/', title: 'Галлерея' },
   { href: '/tags', title: 'Тэги' },
   { href: '/projects', title: 'Проекты' },
   { href: '/about', title: 'О блоге' },
