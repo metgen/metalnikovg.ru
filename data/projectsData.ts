41586de80be6b70bb2637e97bc77107206ec3114
Gennadiy Metalnikov <met_gen@outlook.com>
2024-06-01 15:26:52 +0300

    rollback projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,34 +3,18 @@ import type { Project } from '@/types/data';
 const projectsData: Project[] = [
   {
     type: 'main',
+    title: 'GitBook',
+    description: 'Моя личная база знаний',
+    imgSrc: '/static/images/projects/gitbook.png',
+    url: 'https://gitbook.metalnikovg.ru',
+    builtWith: ['GitBook', 'GitHub', 'Markdown'],
+  },
+  {
+    type: 'self',
     title: 'Персональный веб-сайт',
     imgSrc: '/static/images/projects/personal_blog.png',
     repo: 'metalnikovg.ru',
     builtWith: ['Next.js', 'Tailwind', 'Typescript', 'Prisma', 'Umami'],
-  },
-  {
-    type: 'main',
-    title: 'GitBook',
-    imgSrc: '/static/images/projects/gitbook.png',
-    url: 'https://gitbook.metalnikovg.ru',
-    repo: 'gitbook.metalnikovg',
-    builtWith: ['GitBook', 'GitHub', 'Markdown'],
-  },
-  {
-    type: 'main',
-    title: 'EXIF - галерея',
-    imgSrc: '/static/images/projects/gallery.png',
-    url: 'https://gallery.metalnikovg.ru',
-    repo: 'exif-gallery',
-    builtWith: [ 'Vercel', 'Vercel Postgres', 'GitHub', 'Typescript', 'Cloudflare R2'],
-  },
-  {
-    type: 'self',
-    title: 'MKT-monitoring',
-    description: 'Мониторинг роутера Mikrotik',
-    imgSrc: '/static/images/projects/mkt-monitoring.png',
-    repo: 'mkt_monitoring',
-    builtWith: ['Grafana', 'Prometheus', 'Blackbox', 'Nginx', 'Docker'],
   },
 ];
 
