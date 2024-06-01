42f80ef9361595fca76c1a0a616a662aac3bb4bb
Gennadiy Metalnikov <met_gen@outlook.com>
2024-06-01 18:24:09 +0300

    add favicon.ico path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
       <Html lang={siteMetadata.language} className="scroll-smooth">
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+          <link rel="icon" href="/static/favicons/favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
