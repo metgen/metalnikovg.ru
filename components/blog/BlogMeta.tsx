fd34a8389fbcf93dfbb684ad42c6c3c6f69032e7
Gennadiy <metalnikov.gennadiy@gmail.com>
2024-06-02 09:55:03 +0300

    disable view counts

--- components/blog/BlogMeta.tsx
+++ components/blog/BlogMeta.tsx
@@ -18,11 +18,13 @@ const BlogMeta = ({ date, slug, readingTime }: BlogMetaProps) => {
         <Twemoji emoji="hourglass-not-done" size="1" />
         <span className="ml-1.5 md:ml-2">{Math.ceil(readingTime.minutes)} мин. чтения</span>
       </div>
+      {/* Uncomment to enable view count posts
       <span className="mx-2">{` • `}</span>
       <div className="flex items-center">
         <Twemoji emoji="eye" size="" />
         <ViewCounter className="ml-1.5 md:ml-2" slug={slug} />
       </div>
+      */}
     </dd>
   );
 };
