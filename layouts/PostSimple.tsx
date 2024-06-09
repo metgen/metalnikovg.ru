5ea6a32a1e43b8cea43f6523939dfe9a32bf48f2
Gennadiy <metalnikov.gennadiy@gmail.com>
2024-06-09 22:29:34 +0300

    add view on github link

--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from 'react';
 import { Comments } from 'pliny/comments';
 import { CoreContent } from 'pliny/utils/contentlayer';
-
 import type { Blog } from 'contentlayer/generated';
-
 import siteMetadata from '@/data/siteMetadata';
 import Link from '@/components/Link';
 import PageTitle from '@/components/PageTitle';
@@ -13,6 +11,8 @@ import ScrollTopAndComment from '@/components/ScrollTopAndComment';
 import BlogMeta from '@/components/blog/BlogMeta';
 import BlogTags from '@/components/blog/BlogTags';
 
+const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`;
+
 interface LayoutProps {
   content: CoreContent<Blog>;
   children: ReactNode;
@@ -21,7 +21,7 @@ interface LayoutProps {
 }
 
 export default function PostLayout({ content, next, prev, children }: LayoutProps) {
-  const { path, slug, date, title, tags, readingTime } = content;
+  const { filePath, path, slug, date, title, tags, readingTime } = content;
 
   return (
     <SectionContainer>
@@ -46,9 +46,10 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
             </div>
           </header>
 
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0">
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pb-8 pt-10 dark:prose-dark">{children}</div>
+          <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+            <div className="prose max-w-none pb-8 pt-10 dark:prose-dark">{children}</div>
+            <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
+              <Link href={editUrl(filePath)}>Показать на GitHub</Link>
             </div>
             {siteMetadata.comments && (
               <div className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300" id="comment">
