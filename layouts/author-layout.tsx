f9b38c3ed001f56a8189b2b41a26cceb11273e6c
metgen <metalnikov.gennadiy@gmail.com>
2025-01-02 08:00:26 +0300

    feat(about): moving down the contact block

--- layouts/author-layout.tsx
+++ layouts/author-layout.tsx
@@ -15,7 +15,7 @@ import {
   IconBrandGithub,
   IconBrandInstagram,
   IconMail,
-} from '@tabler/icons-react';
+} from '@tabler/icons-react'
 
 interface Props {
   children?: ReactNode
@@ -23,8 +23,7 @@ interface Props {
 }
 
 export function AuthorLayout({ children, content }: Props) {
-  const { name, email, discription } =
-    content
+  const { name, email, discription } = content
   return (
     <Container className="pt-4 lg:pt-12">
       <PageHeader
@@ -35,9 +34,9 @@ export function AuthorLayout({ children, content }: Props) {
       <div className="py-8 md:grid md:grid-cols-3">
         <div className="pr-4">
           <ProfileCard />
-          <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">Социальные сети:</h3>
+          {/* <div className="pb-2 pt-4 lg:pt-12 text-2xl font-bold leading-8 tracking-tight">Социальные сети:</div>
           <SocialAccounts />
-          {/* <Image
+          <Image
             src={'/static/images/avatar.jpg'}
             alt="avatar"
             width={192}
@@ -77,60 +76,79 @@ export function AuthorLayout({ children, content }: Props) {
                 Меня зовут <strong>Геннадий</strong>, и это мой цифровой сад{' '}
               </p>
               <p>
-                Работаю в сефре IT. Мне нравится учиться и получать новые знания в этой области. Веду личную базу знаний в Obsidian, делаю полезные заметки для себя, и публикую их здесь.
+                Работаю в сефре IT. Мне нравится учиться и получать новые знания в этой области.
+                Веду личную базу знаний в Obsidian, делаю полезные заметки для себя, и публикую их
+                здесь.
               </p>
-                Интересные для меня направления:
-                <li>Администрирование: Linux, Windows, Mac OS</li>
-                <li>Автоматизация: bash, powershell скриптинг</li>
-                <li>Оркестрация/контейнеризация: Kubernetes, Portainer, Docker</li>
-                <li>Мониторинг: Prometheus, Zabbix, Grafana</li>
-                <li>Системы виртуализации: Proxmox, KVM, Hyper-V</li>
-                <li>Сети: TCP/IP, DHCP, DNS, VPN, BGP, PROXY</li>
+              Интересные для меня направления:
+              <li>Администрирование: Linux, Windows, Mac OS</li>
+              <li>Автоматизация: bash, powershell скриптинг</li>
+              <li>Оркестрация/контейнеризация: Kubernetes, Portainer, Docker</li>
+              <li>Мониторинг: Prometheus, Zabbix, Grafana</li>
+              <li>Системы виртуализации: Proxmox, KVM, Hyper-V</li>
+              <li>Сети: TCP/IP, DHCP, DNS, VPN, BGP, PROXY</li>
             </div>
             <div>
               <h2>Цель этого сайта</h2>
               <p>
-              Этот сайт создан для публикации некоторых заметок из моей личной базы знаний в Obsidian и представляют собой своего рода "Digital garden"
+                Этот сайт создан для публикации некоторых заметок из моей личной базы знаний в
+                Obsidian и представляют собой своего рода "Digital garden"
               </p>
+              <p>Здесь я буду делиться своими заметками на различные темы связанные с IT.</p>
+              Даже если эта информация уже доступна на других ресурсах, то возможно мой способ ее
+              формулирования и подачи может оказаться вам ближе и вы сможете найти здесь что-то
+              полезное для себя.
+              <p>В галерее выкладывыаю свои мобильные фотографии.</p>
+              Цифровые сады — это как личные вики, и этот не исключение. Эти заметки для меня, чтобы
+              помочь мне сохранить информацию и выступают в качестве удобного справочного материала,
+              а также развить новые идеи. Все остальное — просто бонус.
               <p>
-              Здесь я буду делиться своими заметками на различные темы связанные с IT.
-              </p>
-              Даже если эта информация уже доступна на других ресурсах, то возможно мой способ ее формулирования и подачи может оказаться вам ближе и вы сможете найти здесь что-то полезное для себя.
-              <p>
-              В галерее выкладывыаю свои мобильные фотографии.
-              </p>
-              Цифровые сады — это как личные вики, и этот не исключение. Эти заметки для меня, чтобы помочь мне сохранить информацию и выступают в качестве удобного справочного материала, а также развить новые идеи. Все остальное — просто бонус.
-              <p>
-              Буду признателен за ваши комментарии... <Twemoji emoji="clinking-beer-mugs" />.
+                Буду признателен за ваши комментарии... <Twemoji emoji="clinking-beer-mugs" />.
               </p>
             </div>
             <div>
               <h2>Концепция сайта</h2>
               <p>
-              Это статический сайт с генератором страниц на современном техническом стеке. Он имеет следующие преимущества:
+                Это статический сайт с генератором страниц на современном техническом стеке. Он
+                имеет следующие преимущества:
               </p>
-              
-              <li>Публикации в формате <strong>MARKDOWN</strong>. (Позволяют легко публиковать заметки из <strong>Obsidian</strong>)</li>
-              
+
+              <li>
+                Публикации в формате <strong>MARKDOWN</strong>. (Позволяют легко публиковать заметки
+                из <strong>Obsidian</strong>)
+              </li>
+
               <li>Отсутствие базы данных (упрощает хранение и обслуживание)</li>
-              
-              <li>Исходный код и содержимое сайта хранятся в репозитории <strong>GitHub</strong>.</li>
-              
-              <li>Пайплайн CI/CD при помощи GitHub Actions (автоматический деплой из репозитория после каждого <code>git push</code>)</li>
-              
-              <li>Сайт имеет почти идеальную оценку произвольности <strong>Lighthouse</strong>.</li>
+
+              <li>
+                Исходный код и содержимое сайта хранятся в репозитории <strong>GitHub</strong>.
+              </li>
+
+              <li>
+                Пайплайн CI/CD при помощи GitHub Actions (автоматический деплой из репозитория после
+                каждого <code>git push</code>)
+              </li>
+
+              <li>
+                Сайт имеет почти идеальную оценку произвольности <strong>Lighthouse</strong>.
+              </li>
 
               <img src="/static/images/ligthouse_report.png"></img>
 
               <li>Имеется светлая и темная тема, а так же удобный мобильный вид.</li>
               <li>Хорошая SEO оптимизация.</li>
-              <li>Для отображение блоков кода и подсветки синтаксиса используется <a href="https://github.com/timlrx/rehype-prism-plus">rehype-prism-plus</a></li>
-              <li>Для комментариев используется <a href="https://github.com/giscus/giscus">giscus</a></li>
+              <li>
+                Для отображение блоков кода и подсветки синтаксиса используется{' '}
+                <a href="https://github.com/timlrx/rehype-prism-plus">rehype-prism-plus</a>
+              </li>
+              <li>
+                Для комментариев используется <a href="https://github.com/giscus/giscus">giscus</a>
+              </li>
             </div>
             <div>
               <h2>Технический стек</h2>
               <p>
-                  Сайт использует в качестве хостинга{' '}
+                Сайт использует в качестве хостинга{' '}
                 <a href="https://vercel.com/" target="_blank">
                   Vercel
                 </a>
@@ -138,11 +156,15 @@ export function AuthorLayout({ children, content }: Props) {
                 <a href="https://nextjs.org/" target="_blank">
                   Next.js
                 </a>{' '}
-                , {' '}
+                ,{' '}
                 <a href="https://tailwindcss.com/" target="_blank">
                   Tailwind CSS
                 </a>{' '}
-                и шаблона <a href="https://tailwind-nextjs-starter-blog.vercel.app/">Tailwind Nextjs Starter Blog</a>.
+                и шаблона{' '}
+                <a href="https://tailwind-nextjs-starter-blog.vercel.app/">
+                  Tailwind Nextjs Starter Blog
+                </a>
+                .
               </p>
               <p>
                 Некоторые идеи были позаимствованы у{' '}
@@ -164,14 +186,16 @@ export function AuthorLayout({ children, content }: Props) {
                   </a>{' '}
                 </li>
                 <li>
-                  <Twemoji emoji="eyes" /> Внесены небольшие изменения в тему, иконки и структуру сайта.
+                  <Twemoji emoji="eyes" /> Внесены небольшие изменения в тему, иконки и структуру
+                  сайта.
                 </li>
                 <li>
-                  <Twemoji emoji="man-technologist" /> Этот веб-сайт использует дизайн и код из репозитория{' '}
+                  <Twemoji emoji="man-technologist" /> Этот веб-сайт использует дизайн и код из
+                  репозитория{' '}
                   <a href="https://github.com/timlrx/tailwind-nextjs-starter-blog" target="_blank">
                     tailwind-nextjs-starter-blog
                   </a>{' '}
-                    и включает шаблон tailwind-nextjs-starter-blog, версии 2.0.
+                  и включает шаблон tailwind-nextjs-starter-blog, версии 2.0.
                 </li>
                 {/* <li>
                   <Twemoji emoji="inbox-tray" /> Bumping up <code>mdx-bundler</code>,{' '}
@@ -180,25 +204,25 @@ export function AuthorLayout({ children, content }: Props) {
                 </li> */}
               </ul>
               <p>
-                  Мой{' '}
+                Мой{' '}
                 <a href="https://github.com/metgen/metalnikovg.ru" target="_blank">
                   Github репозиторий
                 </a>{' '}
-                  для этого сайта.
+                для этого сайта.
               </p>
             </div>
             <div>
               <h2>Ресурсы</h2>
               <p>
-                  Большинство изображений взяты из{' '}
+                Большое количество изображений взято из{' '}
                 <a href="https://unsplash.com/" target="_blank">
                   Unsplash
                 </a>
-                , гифки из{' '}
-                <a href="https://giphy.com/" target="_blank">
-                  GIPHY
+                , иконок из{' '}
+                <a href="https://www.svgrepo.com/" target="_blank">
+                  SVG Repo
                 </a>
-                , а иллюстрации из{' '}
+                , а иллюстраций из{' '}
                 <a href="https://storyset.com/" target="_blank">
                   Storyset
                 </a>
@@ -208,9 +232,13 @@ export function AuthorLayout({ children, content }: Props) {
                 Спасибо за бесплатные ресурсы <Twemoji emoji="folded-hands" />.
               </p>
             </div>
+            <div>
+              <h2>Контакты</h2>
+              <SocialAccounts />
             </div>
           </div>
         </div>
+      </div>
     </Container>
   )
 }
