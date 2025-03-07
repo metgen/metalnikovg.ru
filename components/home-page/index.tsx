2547a7771c2c9c5e9c74de58e645ceea5af3fe3e
metgen <metalnikov.gennadiy@gmail.com>
2025-03-07 22:49:41 +0300

    feat(homepage): disable snowfall on homepage

--- components/home-page/index.tsx
+++ components/home-page/index.tsx
@@ -11,7 +11,7 @@ import { LatestPosts } from './latest-posts'
 import { TypedBios } from './typed-bios'
 import PopularTags from '~/components/home-page/populartags'
 import BlogHome from '~/icons/blog.svg'
-import Snowfall from 'react-snowfall'
+// import Snowfall from 'react-snowfall'
 
 export function Home({
   posts,
@@ -22,6 +22,7 @@ export function Home({
 }) {
   return (
     <Container as="div">
+      {/*
       <div className="relative">
         <Snowfall
           snowflakeCount={60}
@@ -32,28 +33,31 @@ export function Home({
             height: '100vh',
           }}
         />
-        <div className="py-6 md:pb-8 xl:grid xl:grid-cols-3">
-          <div className="space-y-4 md:space-y-6 md:pr-8 xl:col-span-2">
-            <Greeting />
-            <div className="text-base leading-7 text-gray-600 dark:text-gray-400 md:text-lg md:leading-8">
-              <Intro />
-              <TypedBios />
-              <div className="mb-6 mt-4 md:mb-8">
-                <p>
-                  Даже если эта информация уже доступна на других ресурсах, то возможно мой способ
-                  ее формулирования и подачи может оказаться вам ближе и вы сможете найти здесь
-                  что-то полезное для себя.
-                </p>
-              </div>
-              {/* <BlogLinks /> */}
+*/}
+      <div className="py-6 md:pb-8 xl:grid xl:grid-cols-3">
+        <div className="space-y-4 md:space-y-6 md:pr-8 xl:col-span-2">
+          <Greeting />
+          <div className="text-base leading-7 text-gray-600 dark:text-gray-400 md:text-lg md:leading-8">
+            <Intro />
+            <TypedBios />
+            <div className="mb-6 mt-4 md:mb-8">
+              <p>
+                Даже если эта информация уже доступна на других ресурсах, то возможно мой способ ее
+                формулирования и подачи может оказаться вам ближе и вы сможете найти здесь что-то
+                полезное для себя.
+              </p>
             </div>
-          </div>
-          <div className="hidden pl-4 pt-8 xl:block">
-            {/* <ProfileCard /> */}
-            <BlogHome className="mx-auto aspect-square w-[100px] max-w-[80vw] md:w-[300px]" />
+            {/* <BlogLinks /> */}
           </div>
         </div>
+        <div className="hidden pl-4 pt-8 xl:block">
+          {/* <ProfileCard /> */}
+          <BlogHome className="mx-auto aspect-square w-[100px] max-w-[80vw] md:w-[300px]" />
+        </div>
       </div>
+      {/*
+      </div>
+*/}
       <PopularTags />
       <LatestPosts posts={posts} snippets={snippets} />
       {/* {SITE_METADATA.newsletter?.provider && (
